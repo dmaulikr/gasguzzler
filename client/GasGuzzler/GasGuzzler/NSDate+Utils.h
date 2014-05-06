0a63a2ad0f674134bd00ccdf5b79ba9761858e90
Raymond Kennedy <raymondk@stanford.edu>
2014-05-05 21:27:42 -0700

    Added some grouping in the Xcode proj

---
+++ client/GasGuzzler/GasGuzzler/NSDate+Utils.h
@@ -0,0 +1,13 @@
+//
+//  NSDate+Utils.h
+//  GasGuzzler
+//
+//  Created by Raymond kennedy on 5/5/14.
+//  Copyright (c) 2014 Raymond kennedy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (Utils)
+
+@end
---
+++ client/GasGuzzler/GasGuzzler/NSDate+Utils.m
@@ -0,0 +1,13 @@
+//
+//  NSDate+Utils.m
+//  GasGuzzler
+//
+//  Created by Raymond kennedy on 5/5/14.
+//  Copyright (c) 2014 Raymond kennedy. All rights reserved.
+//
+
+#import "NSDate+Utils.h"
+
+@implementation NSDate (Utils)
+
+@end
